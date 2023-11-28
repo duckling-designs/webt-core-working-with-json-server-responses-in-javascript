af685e2d87d8fcd730880367e3a746d9c2fa7dcf
Lisa-Marie <0198@htl.rennweg.at>
2023-11-28 09:24:21 +0100

    finished US1 Wedding CORE

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function fetchDummyData() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        document.getElementById("dummy").innerHTML =
-            this.responseText;
-    }
-    xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
-    xhttp.send();
+async function fetchWeddingLocation() {
+    const response = await fetch('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TRAUMHOCHZEITOGD&srsName=EPSG:4326&outputFormat=json');
+    const weddingLocation = await response.json();
+    console.log(weddingLocation);
+    getRandomLocation(weddingLocation);
+
+}
+
+function getRandomLocation(json) {
+    let locationCount = json["features"].length;
+    let randomID = Math.floor(Math.random() * locationCount) - 1;
+    let output = json["features"][randomID]["properties"];
+    console.log(randomID)
+    document.getElementById("weddingLocation").innerHTML =
+        output["LOCATION"] + " | " + output["ADRESSE"] + " | " + "<a href=\"" + output['WEBLINK1'] + "\">" + "Weblink zum Standesamt" + "</a>";
 }
